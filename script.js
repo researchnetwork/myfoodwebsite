197926999d5e6c608099192575ef577e7056a1fb
Henry Nwaogu <87135571+researchnetwork@users.noreply.github.com>
2022-06-02 11:57:32 +0100

    Added 2 images, html, css and javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// a simple bit of math TO be filled as final project
+
+let quote = "If you believe, nothing is impossible";
+console.log(quote);
+// computations of numbers with srings giving 427 and 35 as values
+let x = "42" + 7;
+
+let y = "42" - 7;
+
+console.log("x = " + x)
+console.log("y = " + y)
+
+// alternating assignments giving the last assignment to the variables
+let codeLove = "cool"
+let coding = "cool";
+coding = "trendy and fun";
+console.log("coding is " + coding)
+console.log("Coding is " + codeLove)
